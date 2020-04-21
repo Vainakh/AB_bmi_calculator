4bf2c9fcd52c3fc8e951ef6531c9dc5becd63cd1
Adlan Yandarbiev <vainakh@MacBook-Pro.local>
2020-04-21 08:40:49 -0700

    Created calculator and bmi calculator using node.ja and express.js

---
+++ calculator.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  let num1 = Number(req.body.n1);
+  let num2 = Number(req.body.n2);
+
+  let result = num1 + num2;
+  res.send("The result of the calculation is: " + result);
+})
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+})
+
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+// app.post("/bmicalculator", function(req, res){
+//   res.send("<h1>BMI Calculator</h1>");
+// });
+
+app.post("/bmicalculator", function(req, res){
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+
+  let bmi = weight / (height * height);
+  res.send("Your BMI is: " + bmi);
+})
